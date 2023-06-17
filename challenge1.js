743a4820ac58fa6697f870afc00c4c6a2210da5c
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-17 11:01:18 +0300

    Added validation which propmpts user for valid marks to grade

--- challenge1.js
+++ challenge1.js
@@ -1,6 +1,8 @@
 let marks = prompt('Enter student marks :')
 let grade;
 
+isValidMarks(marks)
+
 if (marks > 79) { 
     grade = 'A'
 } else if (marks >= 60 && marks <= 79) {
@@ -14,3 +16,14 @@ if (marks > 79) {
 }
 
 console.log(`Grade is ${grade}`)
+
+function isValidMarks(studentMarks) {
+    if (Number.isNaN(Number(studentMarks)) || studentMarks < 0 || studentMarks > 100) {
+        if (confirm('Invalid input. Enter valid student marks (between 1-100). Try again?')) {
+            marks = prompt('Invalid input. Enter valid student marks (between 1-100)')
+            isValidMarks(marks)
+        }
+    }else{
+        marks = parseInt(studentMarks, 10);
+    }
+}

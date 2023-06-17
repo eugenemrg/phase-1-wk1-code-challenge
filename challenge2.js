c4b896cc075a45186f9683990c2ab1e08a3317f7
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-17 12:26:06 +0300

    Add variables for speed limit, demerit bounds, and output message

--- challenge2.js
+++ challenge2.js
@@ -1,14 +1,19 @@
+const speedLimit = 70;
+const demeritBounds = 5;
 let speed = prompt('Enter car speed : ')
+let message;
 
 validateSpeed(speed)
 
+
+
 function validateSpeed(carSpeed) {
-    if(Number.isNaN(Number(carSpeed)) || carSpeed < 0){
-        if(confirm('Invalid car speed. Try again ?')){
+    if (Number.isNaN(Number(carSpeed)) || carSpeed < 0) {
+        if (confirm('Invalid car speed. Try again ?')) {
             speed = prompt('Enter valid car speed : ')
             validateSpeed(speed)
         }
-    }else{
-        speed = parseInt(carSpeed)
+    } else {
+        speed = parseFloat(carSpeed)
     }
 }

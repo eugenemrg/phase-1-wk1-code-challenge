37747eb46eefbb23d0db8e03a087b684b61ca658
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-06-18 11:45:45 +0300

    Initial functions

---
+++ challenge3.js
@@ -0,0 +1,17 @@
+function getNetSalary(employeeBasicSalary, employeeBenefits) {
+
+}
+
+function getNHIFContribution(employeeGrossIncome) {
+
+}
+
+function getNSSFContribution(employeeGrossIncome) {
+
+}
+
+function getPAYE(employeeGrossIncome, ...totalDeductions) {
+
+}
+
+console.log(getNetSalary(10000, 10000))
